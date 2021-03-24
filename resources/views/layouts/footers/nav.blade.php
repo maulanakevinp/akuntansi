c5daaf2554ec08eb743c1be5609145f0808ab975
Maulana Kevin Pradana <44237629+maulanakevinp@users.noreply.github.com>
2021-03-24 09:59:25 +0700

    Update nav.blade.php

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,5 +1,13 @@
-    <div class="copyright text-center text-xl-left text-muted">
-        &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
-        <a target="_blank" href="https://github.com/maulanakevinp/akuntansi" class="font-weight-bold ml-1">Sistem Informasi Akuntansi</a> 
-        By <a href="https://maulanakevinp.github.io" target="_blank">Maulana Kevin Pradana</a>
+<div class="row">
+    <div class="col-md-6">
+        <div class="copyright text-center text-xl-left text-muted">
+            &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a>
+        </div>
     </div>
+    <div class="col-md-6">
+        <div class="copyright text-center text-xl-right text-muted">
+            <a target="_blank" href="https://github.com/maulanakevinp/akuntansi" class="font-weight-bold ml-1">Sistem Informasi Akuntansi</a> 
+            By <a href="https://maulanakevinp.github.io" target="_blank">Maulana Kevin Pradana</a>
+        </div>
+    </div>
+</div>
