dd49fce74663ffbe81821401e40d417c486831bf
Maulana Kevin Pradana <44237629+maulanakevinp@users.noreply.github.com>
2021-03-24 09:40:14 +0700

    Update nav.blade.php

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,27 +1,5 @@
-<div class="row align-items-center justify-content-xl-between">
-    <div class="col-xl-4">
-        <div class="copyright text-center text-xl-left text-muted">
-            &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
-            <a href="https://github.com/maulanakevinp/akuntansi" class="font-weight-bold ml-1">Kelompok 8 DDA A</a>
-        </div>
+    <div class="copyright text-center text-xl-left text-muted">
+        &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
+        <a target="_blank" href="https://github.com/maulanakevinp/akuntansi" class="font-weight-bold ml-1">Sistem Informasi Akuntansi</a> 
+        By <a href="https://maulanakevinp.github.io" target="_blank">Maulana Kevin Pradana</a>
     </div>
-    <div class="col-xl-8">
-        <ul class="nav nav-footer justify-content-center justify-content-xl-end">
-            <li class="nav-item">
-                <a href="https://maulanakevinp.github.io" class="nav-link" target="_blank">Maulana Kevin P</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.instagram.com/bagasdika22/" class="nav-link" target="_blank">Bagas Suryadika M</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.instagram.com/afebri16/" class="nav-link" target="_blank">Arif Febrian</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.instagram.com/nhmcwt/" class="nav-link" target="_blank">Nur Hidayah M</a>
-            </li>
-            <li class="nav-item">
-                <a href="https://www.instagram.com/sindiyulitaa/" class="nav-link" target="_blank">Sindi Yulita A</a>
-            </li>
-        </ul>
-    </div>
-</div>
